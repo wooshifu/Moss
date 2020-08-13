15bed912822d0edb03f70771d7e9d8720c48d9b6
shifu <shifu.wu@outlook.com>
2020-08-13 22:25:20 +0800

    [log] remove debug log

--- SourceCode/Kernel/src/init.c
+++ SourceCode/Kernel/src/init.c
@@ -1,13 +1,7 @@
-#include "log.h"
 #include "logo.h"
 #include "uart.h"
 
 void kernel_main(void) {
   init_uart();
   print_moss_logo();
-
-  log_i("hello %d", 123);
-  log_i("hello %s", "world");
-  log_i("hello 0x%x", 12);
-  log_i("hello 0X%X", 12);
 }
