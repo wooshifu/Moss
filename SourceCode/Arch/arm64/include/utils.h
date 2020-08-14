b015ad1b141cb144a3a7775e65991dc3042d8a2d
shifu <shifu.wu@outlook.com>
2020-08-15 00:04:56 +0800

    [boot] add get_current_el

---
+++ SourceCode/Arch/arm64/include/utils.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int get_current_el();
--- SourceCode/Kernel/src/init.c
+++ SourceCode/Kernel/src/init.c
@@ -1,7 +1,11 @@
+#include "log.h"
 #include "logo.h"
 #include "uart.h"
+#include "utils.h"
 
 void kernel_main(void) {
   init_uart();
   print_moss_logo();
+
+  log_i("current el: %d", get_current_el());
 }
