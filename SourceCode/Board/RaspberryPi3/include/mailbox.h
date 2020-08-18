d22fad568531a74c87c0c8f318040839c5b10f1f
shifu <shifu.wu@outlook.com>
2020-08-18 21:43:34 +0800

    [mailbox] refactor uart0 implementation

--- SourceCode/Board/RaspberryPi3/include/mailbox.h
+++ SourceCode/Board/RaspberryPi3/include/mailbox.h
@@ -1,35 +1,156 @@
-#if 0
 #pragma once
 
-/* a properly aligned buffer */
-/* mailbox message buffer */
-volatile unsigned int __attribute__((aligned(16))) mailbox[36];
+#include <stdbool.h>
+#include <stdint.h>
 
-#define MAILBOX_REQUEST 0
+#include "gpio.h"
+
+/**
+ *        0       4       8      12      16      20      24      28      32
+ *        +-------+-------------------------------------------------------+
+ * 0x00   |rd chn |                     read data                         |
+ *        +-------+-------------------------------------------------------+
+ * 0x04   |                             Unused                            |
+ * ...    \\                                                             \\
+ * 0x14   |                             Unused                            |
+ *        +-----------------------------------------------------------+-+-+
+ * 0x18   |      status reserved                                      |E|F|
+ *        +-----------------------------------------------------------+-+-+
+ * 0x1C   |                             Unused                            |
+ *        +-----------------------------------------------------------+-+-+
+ * 0x20   |wt chn |                    write data                         |
+ *        +-----------------------------------------------------------+-+-+
+ */
+#define VIDEOCORE_MBOX (MMIO_BASE + 0x0000B880)
+
+#define MAILBOX_CHANNEL_POWER_MANAGEMENT 0
+#define MAILBOX_CHANNEL_FRAMEBUFFER 1
+#define MAILBOX_CHANNEL_VIRTUAL_UART 2
+#define MAILBOX_CHANNEL_VCHIQ 3
+#define MAILBOX_CHANNEL_LEDS 4
+#define MAILBOX_CHANNEL_BUTTONS 5
+#define MAILBOX_CHANNEL_TOUCH_SCREEN 6
+#define MAILBOX_CHANNEL_COUNT 7
+#define MAILBOX_CHANNEL_PROPERTY_TAGS_ARM_TO_VIDEO_CORE 8
+#define MAILBOX_CHANNEL_PROPERTY_TAGS_VIDEO_CORE_TO_ARM 9
+
+/**
+ * The read register for mailbox 0 at offset (the Linux source mentions something of "and the next 4 words", but I've
+ * found it sufficient to read only from this address) 0-3(channel):	The mailbox channel number from which the data
+ * originated 4-31(data):	The 28 bits of data sent to the CPU
+ */
+#define MAILBOX0_READ ((volatile uint32_t *)(VIDEOCORE_MBOX + 0x0))
+
+// Read from the mailbox without removing data from it.
+#define MAILBOX0_PEAK ((volatile uint32_t *)(VIDEOCORE_MBOX + 0x10))
+
+// Sender ID (bottom 2 bits only)
+#define MAILBOX0_SENDER ((volatile uint32_t *)(VIDEOCORE_MBOX + 0x14))
+
+/**
+ * The status register for mailbox 0
+ * 0-29:	N/A	Not documented here. Unused?
+ * 30:	MAIL_EMPTY	Set if the mailbox is empty, and thus no more data is available to be read from it.
+ * 31:	MAIL_FULL	Set if the mailbox is full, and thus no more data can be written to it.
+ */
+#define MAILBOX0_STATUS ((volatile uint32_t *)(VIDEOCORE_MBOX + 0x18))
+
+// The configuration register for mailbox 0
+#define MAILBOX0_CONFIG ((volatile uint32_t *)(VIDEOCORE_MBOX + 0x1C))
+
+/**
+ *  The write register for mailbox 0 (this is actually the read register for mailbox 1).
+ *  0-3:	channel	The mailbox channel number to which the data is to be sent
+ *  4-31:	data	The 28 bits of data to be sent to the destination
+ */
+#define MAILBOX0_WRITE ((volatile uint32_t *)(VIDEOCORE_MBOX + 0x20))
+
 #define MAILBOX_CODE_REQUEST 0x00000000
 #define MAILBOX_CODE_RESPONSE_SUCCESS 0x80000000
 #define MAILBOX_CODE_RESPONSE_FAILURE 0x80000001
 
-/* channels */
-#define MAILBOX_CHANNEL_POWER 0
-#define MAILBOX_CHANNEL_FRAMEBUFFER 1
-#define MAILBOX_CHANNEL_VUART 2
-#define MAILBOX_CHANNEL_VCHIQ 3
-#define MAILBOX_CHANNEL_LEDS 4
-#define MAILBOX_CHANNEL_BUTTONS 5
-#define MAILBOX_CHANNEL_TOUCH 6
-#define MAILBOX_CHANNEL_COUNT 7
-#define MAILBOX_CHANNEL_PROPERTY_ARM_TO_VIDEO_CORE 8
-#define MAILBOX_CHANNEL_PROPERTY_VIDEO_CORE_TO_ARM 9
+//#define MAILBOX_CODE_TAG_REQUEST 0x00000000
+#define MAILBOX_CODE_TAG_IS_REQUEST(code) ((code)&0x00000000)
+//#define MAILBOX_CODE_TAG_RESPONSE 0x80000000
+#define MAILBOX_CODE_TAG_IS_RESPONSE(code) ((code)&0x80000000)
 
-/* tags */
-#define MAILBOX_TAG_GET_SERIAL 0x10004
-#define MAILBOX_TAG_SET_POWER 0x28001
-#define MAILBOX_TAG_SET_CLOCK_RATE 0x38002
-#define MAILBOX_TAG_LAST 0
+#define MAILBOX_FULL 1 << 31
+#define MAILBOX_EMPTY 1 << 30
 
-int mailbox_call(unsigned char channel);
+/**
+ * all property interface can be found at below link:
+ * https://github.com/raspberrypi/firmware/wiki/Mailbox-property-interface
+ *
+ *        0       4       8      12      16      20      24      28      32
+ *        +---------------------------------------------------------------+
+ * 0x00   |                         Buffer Size                           |
+ *        +---------------------------------------------------------------+
+ * 0x04   |                   Request/Response Code                       |
+ *        +---------------------------------------------------------------+
+ * 0x08   |                             Tags                              |
+ * ...    \\                                                             \\
+ * 0xXX   |                             Tags                              |
+ *        +---------------------------------------------------------------+
+ * 0xXX+4 |                           End Tag (0)                         |
+ *        +---------------------------------------------------------------+
+ * 0xXX+8 |                           Padding                             |
+ * ...    \\                                                             \\
+ * 0xXX+16|                           Padding                             |
+ *        +---------------------------------------------------------------+
+ *
+ * A Tag starts with a 4 byte tag id, which identifies which command this tag wants to run.
+ * In general, Tags have form 0x000XYZZZ,
+ * where X identifies which hardware device you are accessing,
+ * Y identifies which type of command it is (0 = get, 4 = test, 8 = set),
+ * and ZZZ identifies the specific command.
+ *
+ * map of tag:
+ *        0       4       8      12      16      20      24      28      32
+ *        +---------------------------------------------------------------+
+ * 0x00   |                         Tag Identity                          |
+ *        +---------------------------------------------------------------+
+ * 0x04   |                       Value Buffer Size                       |
+ *        +---------------------------------------------------------------+
+ * 0x08   |                     Request/Response Code                     |
+ *        +---------------------------------------------------------------+
+ * 0x0C   |                          Value Buffer                         |
+ * ...    \\                                                             \\
+ * 0xXX   | Value Buffer  |                    Padding                    |
+ *        +---------------------------------------------------------------+
+ */
+#define PROPERTY_TAG_END 0x00000000
 
-void mailbox_get_serial_number();
+#define MAILBOX_CLOCK_ID_RESERVED 0x000000000  // reserved
+#define MAILBOX_CLOCK_ID_EMMC 0x000000001      // EMMC
+#define MAILBOX_CLOCK_ID_UART 0x000000002      // UART
+#define MAILBOX_CLOCK_ID_ARM 0x000000003       // ARM
+#define MAILBOX_CLOCK_ID_CORE 0x000000004      // CORE
+#define MAILBOX_CLOCK_ID_V3D 0x000000005       // V3D
+#define MAILBOX_CLOCK_ID_H264 0x000000006      // H264
+#define MAILBOX_CLOCK_ID_ISP 0x000000007       // ISP
+#define MAILBOX_CLOCK_ID_SDRAM 0x000000008     // SDRAM
+#define MAILBOX_CLOCK_ID_PIXEL 0x000000009     // PIXEL
+#define MAILBOX_CLOCK_ID_PWM 0x00000000a       // PWM
+#define MAILBOX_CLOCK_ID_HEVC 0x00000000b      // HEVC
+#define MAILBOX_CLOCK_ID_EMMC2 0x00000000c     // EMMC2
+#define MAILBOX_CLOCK_ID_M2MC 0x00000000d      // M2MC
+#define MAILBOX_CLOCK_ID_PIXEL_BVB 0x00000000e // PIXEL_BVB
 
-#endif
+#define MAILBOX_PROPERTY_TAG_SET_CLOCK_RATE 0x00038002
+// NOTE: don't change the struct member order, it should be this order described by rasberrypi docs
+typedef struct mailbox_property_set_clock_rate_t {
+  uint32_t size; // =sizeof(struct my_property_set_clock_rate_t);
+  uint32_t code; // =MAILBOX_CODE_REQUEST ;
+
+  uint32_t tag;                // =PROPERTY_TAG_SET_CLOCK_RATE;
+  uint32_t buffer_size;        // =12;
+  uint32_t tag_code;           // =MAILBOX_CODE_REQUEST;
+  uint32_t clock_id;           // =CLOCK_ID_UART;
+  uint32_t rate;               // =4000000;
+  uint32_t skip_setting_turbo; // =0;
+
+  uint32_t end; // =PROPERTY_TAG_END
+} __attribute__((aligned(16))) mailbox_property_set_clock_rate_t;
+
+void mailbox_call(uint32_t channel, void *data);
+bool is_valid_mailbox_response(uint32_t code, uint32_t tag_code);
--- SourceCode/Board/RaspberryPi3/src/uart0.c
+++ SourceCode/Board/RaspberryPi3/src/uart0.c
@@ -1,6 +1,6 @@
 #include "gpio.h"
 #include "log.h"
-#include "mbox.h"
+#include "mailbox.h"
 
 /* PL011 UART registers */
 #define UART0_DATA_REGISTER ((volatile unsigned int *)(MMIO_BASE + 0x00201000))
@@ -19,54 +19,25 @@ void init_uart0() {
   /* initialize UART */
   *UART0_CONTROL_REGISTER = 0; // turn off UART0
 
-  /*
-   */
-  /* set up clock for consistent divisor values */ /*
+  mailbox_property_set_clock_rate_t data = {.size = sizeof(struct mailbox_property_set_clock_rate_t),
+                                            .code = MAILBOX_CODE_REQUEST,
 
-     mailbox[0] = 9 * 4;
-     mailbox[1] = MAILBOX_REQUEST;
-     mailbox[2] = MAILBOX_TAG_SET_CLOCK_RATE; // set clock rate
-     mailbox[3] = 12;                         // value buffer size in bytes
-     mailbox[4] = 8;                          // request code
-     mailbox[5] = 2;                          // UART clock, clock id
-     mailbox[6] = 4000000;                    // 4Mhz, rate (in Hz)
-     mailbox[7] = 0;                          // clear turbo, setting turbo
-     mailbox[8] = MAILBOX_TAG_LAST;
-     int result = mailbox_call(MAILBOX_CHANNEL_PROPERTY_ARM_TO_VIDEO_CORE);
-   */
-                                                   /*  property_set_clock_rate_t set_clock_rate = {
-                                                         .tag = PROPERTY_TAG_SET_CLOCK_RATE,
-                                                         .code = MAILBOX_CODE_REQUEST,
-                                                         .buffer_size = 12,
-                                                         .clock_id = CLOCK_ID_UART,
-                                                         .rate = 4000000,
-                                                         .skip_setting_turbo = 0,
-                                                     };*/
-  log_d("sizeof(property_set_clock_rate_mail_t):%d", sizeof(property_set_clock_rate_mail_t));
-  property_set_clock_rate_mail_t set_clock_rate_mail = {
-      .size = sizeof(property_set_clock_rate_mail_t),
-      .code = MAILBOX_CODE_REQUEST,
-      .property =
-          {
-              .tag = PROPERTY_TAG_SET_CLOCK_RATE,
-              .buffer_size = 12, // todo: should be 32 aligned
-              .code = MAILBOX_CODE_REQUEST,
-              .clock_id = CLOCK_ID_UART,
-              .rate = 4000000,
-              .skip_setting_turbo = 0,
-          },
-      .end = PROPERTY_TAG_END,
-  };
-  //  log_d("addr: %p", &set_clock_rate);
-  mailbox_message_t message = {.channel = MAILBOX_CHANNEL_PROPERTY_TAGS_ARM_TO_VIDEO_CORE,
-                               .data = CAST_TO_MAILBOX_DATA(set_clock_rate_mail)};
-  /*mailbox_message_t response =*/mailbox_send(&message);
-  if (set_clock_rate_mail.code == MAILBOX_CODE_RESPONSE_FAILURE ||
-      set_clock_rate_mail.property.code == MAILBOX_CODE_RESPONSE_FAILURE) {
-    log_e("could not set mailbox, error code: %d, property code: %d", set_clock_rate_mail.code,
-          set_clock_rate_mail.property.code);
+                                            .tag = MAILBOX_PROPERTY_TAG_SET_CLOCK_RATE,
+                                            .buffer_size = 12, // 12 bytes
+                                            .tag_code = MAILBOX_CODE_REQUEST,
+                                            .clock_id = MAILBOX_CLOCK_ID_UART,
+                                            .rate = 4000000,
+                                            .skip_setting_turbo = 0,
+
+                                            .end = PROPERTY_TAG_END};
+  mailbox_call(MAILBOX_CHANNEL_PROPERTY_TAGS_ARM_TO_VIDEO_CORE, &data);
+  log_d("response code: %x, tag code: %x", data.code, data.tag_code);
+  if (!is_valid_mailbox_response(data.code, data.tag_code)) {
+    // NOTE: uart0 not initialized, could we log this message to uart0???
+    log_f("failed to set clock uart rate, code: %u, tag_code: %u", data.code, data.tag_code);
     return;
   }
+  log_d("uart0(id:%u) clock rate initialized to rate:%u", data.clock_id, data.rate);
 
   /* map UART0 to GPIO pins */
   unsigned int r = *GPFSEL1;
