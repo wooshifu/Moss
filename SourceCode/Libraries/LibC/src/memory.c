a90968f06c821cb8baac9a951a269985ffd6c8e2
shifu <shifu.wu@outlook.com>
2020-08-31 00:27:16 +0800

    [libc] remove unnecessary semicolon

--- SourceCode/Libraries/LibC/src/memory.c
+++ SourceCode/Libraries/LibC/src/memory.c
@@ -1,11 +1,11 @@
 #include "memory.h"
 
-void memory_write_8bits(uint8_t *address, uint8_t value) { *address = value; };
-void memory_write_16bits(uint16_t *address, uint16_t value) { *address = value; };
-void memory_write_32bits(uint32_t *address, uint32_t value) { *address = value; };
-void memory_write_64bits(uint64_t *address, uint64_t value) { *address = value; };
+void memory_write_8bits(uint8_t *address, uint8_t value) { *address = value; }
+void memory_write_16bits(uint16_t *address, uint16_t value) { *address = value; }
+void memory_write_32bits(uint32_t *address, uint32_t value) { *address = value; }
+void memory_write_64bits(uint64_t *address, uint64_t value) { *address = value; }
 
-uint8_t memory_read_8bits(const uint8_t *address) { return *address; };
-uint16_t memory_read_16bits(const uint16_t *address) { return *address; };
-uint32_t memory_read_32bits(const uint32_t *address) { return *address; };
-uint64_t memory_read_64bits(const uint64_t *address) { return *address; };
+uint8_t memory_read_8bits(const uint8_t *address) { return *address; }
+uint16_t memory_read_16bits(const uint16_t *address) { return *address; }
+uint32_t memory_read_32bits(const uint32_t *address) { return *address; }
+uint64_t memory_read_64bits(const uint64_t *address) { return *address; }
