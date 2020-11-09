10f09714333486ff4be14e968603be6705c4fe1e
shifu <shifu.wu@outlook.com>
2020-11-09 23:01:21 +0800

    [bug] revert remove alignment code

--- SourceCode/Board/RaspberryPi3/include/rpi3/mailbox.h
+++ SourceCode/Board/RaspberryPi3/include/rpi3/mailbox.h
@@ -138,7 +138,7 @@
 #define MAILBOX_PROPERTY_TAG_SET_CLOCK_RATE 0x00038002
 // NOTE: don't change the struct member order, it should be this order described by rasberrypi docs
 constexpr int mailbox_property_alignment = 16;
-using mailbox_property_set_clock_rate_t = struct mailbox_property_set_clock_rate_t {
+using mailbox_property_set_clock_rate_t  = struct mailbox_property_set_clock_rate_t {
   u32 size; // =sizeof(struct my_property_set_clock_rate_t);
   u32 code; // =MAILBOX_CODE_REQUEST ;
 
@@ -150,7 +150,7 @@ using mailbox_property_set_clock_rate_t = struct mailbox_property_set_clock_rate
   u32 skip_setting_turbo; // =0;
 
   u32 end; // =MAILBOX_PROPERTY_TAG_END
-};
+} __attribute__((aligned(mailbox_property_alignment)));
 // The buffer itself is 16-byte aligned as only the upper 28 bits of the address can be passed via the mailbox.
 
 /**
