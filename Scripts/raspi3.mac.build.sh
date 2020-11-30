28c8b1ae7952eecf36a5a5b139319f703df3349f
shifu <shifu.wu@outlook.com>
2020-12-01 00:19:41 +0800

    [scripts] scripts refactor

---
+++ Scripts/raspi3.mac.build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+THIS_DIR=$(realpath "$(dirname "$0")")
+echo "current dir: ${THIS_DIR}"
+
+BUILD_DIR="${THIS_DIR}"/output/Raspi3/mac
+rm -rf "${BUILD_DIR}"
+mkdir -p "${BUILD_DIR}"
+
+cd "${BUILD_DIR}" || exit
+cmake -DCMAKE_TOOLCHAIN_FILE="${THIS_DIR}"/../CMake/Toolchain/ToolchainClang.cmake -DARCH=arm64 "${THIS_DIR}/.."
+make
+cd - || exit
--- Scripts/raspi3.mac.build_and_run.sh
+++ Scripts/raspi3.mac.build_and_run.sh
@@ -3,14 +3,9 @@
 THIS_DIR=$(realpath "$(dirname "$0")")
 echo "current dir: ${THIS_DIR}"
 
-BUILD_DIR="${THIS_DIR}"/output/Raspi3/mac
-rm -rf "${BUILD_DIR}"
-mkdir -p "${BUILD_DIR}"
+"${THIS_DIR}"/raspi3.mac.build.sh
 
-cd "${BUILD_DIR}" || exit
-cmake -DCMAKE_TOOLCHAIN_FILE="${THIS_DIR}"/../CMake/Toolchain/ToolchainMacArm64.cmake -DARCH=arm64 "${THIS_DIR}/.."
-make
-cd - || exit
+BUILD_DIR="${THIS_DIR}"/output/Raspi3/mac
 
 echo ">>>>>>>>>>>>>>>> RUNNING KERNEL <<<<<<<<<<<<<<<<"
 cmd="qemu-system-aarch64 -M raspi3 -kernel ${BUILD_DIR}/bin/Kernel.elf -serial mon:stdio -nographic"
