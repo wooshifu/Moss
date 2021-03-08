bc90f96525a5960ca6b03889fa1e48663a6156bc
shifu <shifu.wu@outlook.com>
2021-03-08 18:08:12 +0800

    [stdcxx] interim solution to include aarch64-unknown-elf header files

--- CMake/Setup/HeaderFiles.cmake
+++ CMake/Setup/HeaderFiles.cmake
@@ -7,6 +7,8 @@ function(target_include_libcxx_header_files IN_target)
 endfunction()
 
 function(target_include_libstdcxx_header_files IN_target)
+    # todo: interim solution, refactor this
+    include_directories(${MOSS_SOURCE_CODE_DIR}/Libraries/LibGccStd/include/aarch64-unknown-elf)
     target_include_directories(${IN_target} PRIVATE ${MOSS_SOURCE_CODE_DIR}/Libraries/LibGccStd/include)
 endfunction()
 
