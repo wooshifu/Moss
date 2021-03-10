1dea72406ab6b16642af4d7b9ff13a8afe3d9b61
shifu <shifu.wu@outlook.com>
2021-03-10 23:34:18 +0800

    [error code] error code refactor

--- SourceCode/Libraries/LibCXX/include/libcxx/error_code.hh
+++ SourceCode/Libraries/LibCXX/include/libcxx/error_code.hh
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "limits" // for numeric_limits
+#include "concepts"
+#include "limits"
 
 enum class KErrorCode : int {
   OK = 0,
@@ -8,5 +9,12 @@ enum class KErrorCode : int {
   UNKNOWN = std::numeric_limits<int>::min(),
 };
 
-template <typename T> bool ok(T code);
-template <typename T> bool not_ok(T code);
+inline bool ok(KErrorCode code) { return code == KErrorCode::OK; }
+inline bool not_ok(KErrorCode code) { return not ok(code); }
+
+inline bool ok(bool code) { return code; }
+inline bool not_ok(bool code) { return not ok(code); }
+/// convention: zero represents ok, non-zero represents not ok
+template <typename T> concept Number = std::integral<T> && not std::same_as<T, bool>;
+inline bool ok(Number auto code) { return code == 0; }
+inline bool not_ok(Number auto code) { return not ok(code); }
