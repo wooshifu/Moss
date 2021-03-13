e84a99801cb4448145ad9d4d8f6f7d17fe465350
shifu <shifu.wu@outlook.com>
2021-03-13 14:11:41 +0800

    [memory.cc] inline memory_xxx

--- SourceCode/Libraries/LibCXX/include/libcxx/memory.hh
+++ SourceCode/Libraries/LibCXX/include/libcxx/memory.hh
@@ -2,12 +2,12 @@
 
 #include "libcxx/types.hh"
 
-void memory_write_8bits(u8* address, u8 value);
-void memory_write_16bits(u16* address, u16 value);
-void memory_write_32bits(u32* address, u32 value);
-void memory_write_64bits(u64* address, u64 value);
+inline void memory_write_8bits(u8* address, u8 value) { *address = value; }
+inline void memory_write_16bits(u16* address, u16 value) { *address = value; }
+inline void memory_write_32bits(u32* address, u32 value) { *address = value; }
+inline void memory_write_64bits(u64* address, u64 value) { *address = value; }
 
-u8 memory_read_8bits(const u8* address);
-u16 memory_read_16bits(const u16* address);
-u32 memory_read_32bits(const u32* address);
-u64 memory_read_64bits(const u64* address);
+inline u8 memory_read_8bits(const u8* address) { return *address; }
+inline u16 memory_read_16bits(const u16* address) { return *address; }
+inline u32 memory_read_32bits(const u32* address) { return *address; }
+inline u64 memory_read_64bits(const u64* address) { return *address; }
