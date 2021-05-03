92774d671509c0c6e781ae86b48887d9a2cfecd7
shifu <shifu.wu@outlook.com>
2021-05-03 17:04:37 +0800

    [iwyu] make iwyu a git submodule

--- CMake/Setup/IWYU.cmake
+++ CMake/Setup/IWYU.cmake
@@ -22,11 +22,15 @@ else ()
     endif ()
     message(STATUS "found python3: ${Python3_EXECUTABLE}")
 
+    if (NOT EXISTS ${MOSS_SCRIPTS_DIR}/IWYU/include-what-you-use)
+        message(FATAL_ERROR "include-what-you-use dir could not be found, use `git pull --recurse-submodules` to pull submodules")
+    endif ()
+
     add_custom_target(iwyu_fix_includes
             COMMENT "iwyu fixing includes, wait a moment"
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
             DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
-            COMMAND ${Python3_EXECUTABLE} ${MOSS_SCRIPTS_DIR}/IWYU/iwyu_tool.py -p ${CMAKE_BINARY_DIR} -- ${__iwyu_options} > ${CMAKE_BINARY_DIR}/.iwyu_fix_includes || (exit 0)
-            COMMAND ${Python3_EXECUTABLE} ${MOSS_SCRIPTS_DIR}/IWYU/fix_includes.py --reorder --comments < ${CMAKE_BINARY_DIR}/.iwyu_fix_includes || (exit 0)
+            COMMAND ${Python3_EXECUTABLE} ${MOSS_SCRIPTS_DIR}/IWYU/include-what-you-use/iwyu_tool.py -p ${CMAKE_BINARY_DIR} -- ${__iwyu_options} > ${CMAKE_BINARY_DIR}/.iwyu_fix_includes || (exit 0)
+            COMMAND ${Python3_EXECUTABLE} ${MOSS_SCRIPTS_DIR}/IWYU/include-what-you-use/fix_includes.py --reorder --comments < ${CMAKE_BINARY_DIR}/.iwyu_fix_includes || (exit 0)
             )
 endif ()
