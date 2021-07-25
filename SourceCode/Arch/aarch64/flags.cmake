5744010af303c9ac1214cf3da8a5c224d3aaa812
shifu <shifu.wu@outlook.com>
2021-07-25 20:13:45 +0800

    [flag] add todo and -U__linux__

--- SourceCode/Arch/aarch64/flags.cmake
+++ SourceCode/Arch/aarch64/flags.cmake
@@ -12,7 +12,9 @@
 function(get_compiler_flags OUT_flags)
     set(flags "-DARCH_ARM=1 -DARCH_AARCH64=1 -DBITS_64=1 -D__aarch64__=1")
     if (USING_CLANG_COMPILER)
-        set(${OUT_flags} "${flags} --target=aarch64-unknown-linux-elf -march=armv8-a+crc+fp" PARENT_SCOPE)
+        # TODO: set(${OUT_flags} "${flags} --target=aarch64-unknown-none-elf -march=armv8-a+crc+fp" PARENT_SCOPE) will cause lld failed
+        # add -U__linux__ to indicate that this is not a linux system
+        set(${OUT_flags} "${flags} -U__linux__ --target=aarch64-unknown-linux-elf -march=armv8-a+crc+fp" PARENT_SCOPE)
     elseif (USING_GNU_COMPILER)
         set(${OUT_flags} "${flags} -mcpu=cortex-a53" PARENT_SCOPE)
     endif ()
