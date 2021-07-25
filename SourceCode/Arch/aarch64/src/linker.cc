d80e6ab9aa6689e6e78406585dd3adf7fc3b36a9
shifu <shifu.wu@outlook.com>
2021-07-25 20:17:29 +0800

    [linker] remove array dependency

--- SourceCode/Arch/aarch64/src/linker.cc
+++ SourceCode/Arch/aarch64/src/linker.cc
@@ -1,21 +1,17 @@
 #include "aarch64/linker.hh"
 #include "kconfig.hh"
 #include "libcxx/log.hh" // for log_d
-
-//#include <array>   // for array
-//#include <tuple>   // for make_tuple, get
-//#include <utility> // for forward
+#include "tuple"         // for make_tuple, get
 
 void print_memory_layout() {
-#if 0
-  using std::make_tuple;
-  const std::array sections = {
-      make_tuple("text", get_text_start_address(), get_text_end_address(), get_text_size()),
-      make_tuple("init_hook", get_init_hook_start_address(), get_init_hook_end_address(), get_init_hook_size()),
-      make_tuple("rodata", get_rodata_start_address(), get_rodata_end_address(), get_rodata_size()),
-      make_tuple("data", get_data_start_address(), get_data_end_address(), get_data_size()),
-      make_tuple("bss", get_bss_start_address(), get_bss_end_address(), get_bss_size()),
-      make_tuple("stack", get_stack_start_address(), get_stack_end_address(), get_stack_size()),
+#if CONFIG_KERNEL_PRINT_MEMORY_LAYOUT
+  const std::tuple<const char*, u64, u64, u64> sections[] = {
+      std::make_tuple("text", get_text_start_address(), get_text_end_address(), get_text_size()),
+      std::make_tuple("init_hook", get_init_hook_start_address(), get_init_hook_end_address(), get_init_hook_size()),
+      std::make_tuple("rodata", get_rodata_start_address(), get_rodata_end_address(), get_rodata_size()),
+      std::make_tuple("data", get_data_start_address(), get_data_end_address(), get_data_size()),
+      std::make_tuple("bss", get_bss_start_address(), get_bss_end_address(), get_bss_size()),
+      std::make_tuple("stack", get_stack_start_address(), get_stack_end_address(), get_stack_size()),
   };
 
   constexpr const char* cutter_line{"---------------------------------------------------------------------------"};
