c631a740bee77cd2fbe064583e06fa3114b05ed6
shifu <shifu.wu@outlook.com>
2021-10-30 22:14:26 +0800

    [unit] add TiB and PiB

--- SourceCode/Libraries/lib_cxx/include/libcxx/unit.hh
+++ SourceCode/Libraries/lib_cxx/include/libcxx/unit.hh
@@ -23,10 +23,18 @@ static_assert(1_KB == 1000);
 consteval unsigned long operator"" _KiB(unsigned long long KiB) { return 1024 * KiB; }
 static_assert(1_KiB == 1024);
 consteval unsigned long operator"" _MB(unsigned long long MB) { return operator""_KB(1000 * MB); }
-static_assert(1_MB == 1000'000);
+static_assert(1_MB == 1000_KB);
 consteval unsigned long operator"" _MiB(unsigned long long MiB) { return operator""_KiB(1024 * MiB); }
-static_assert(1_MiB == 1024UL * 1024UL);
+static_assert(1_MiB == 1024_KiB);
 consteval unsigned long operator"" _GB(unsigned long long GB) { return operator""_MB(1000 * GB); }
-static_assert(1_GB == 1000'000'000);
+static_assert(1_GB == 1000_MB);
 consteval unsigned long operator"" _GiB(unsigned long long GiB) { return operator""_MiB(1024 * GiB); }
-static_assert(1_GiB == 1024UL * 1024UL * 1024UL);
+static_assert(1_GiB == 1024_MiB);
+consteval unsigned long operator"" _TB(unsigned long long TB) { return operator""_GB(1000 * TB); }
+static_assert(1_TB == 1000_GB);
+consteval unsigned long operator"" _TiB(unsigned long long TiB) { return operator""_GiB(1024 * TiB); }
+static_assert(1_TiB == 1024_GiB);
+consteval unsigned long operator"" _PB(unsigned long long PB) { return operator""_TB(1000 * PB); }
+static_assert(1_PB == 1000_TB);
+consteval unsigned long operator"" _PiB(unsigned long long PiB) { return operator""_TiB(1024 * PiB); }
+static_assert(1_PiB == 1024_TiB);
