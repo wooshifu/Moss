a860fb8e613b5b2f2793a807dc543d791736136c
shifu <shifu.wu@outlook.com>
2022-06-17 13:34:43 +0800

    [main test] access global variable after mmu enabled

--- SourceCode/Kernel/src/main.cc
+++ SourceCode/Kernel/src/main.cc
@@ -1,9 +1,12 @@
 #include "libcxx/attr.hh"
 #include "libcxx/macros.hh"
+#include "libcxx/types.hh" // for u64
+
+u64 xxx = 0x1122334455667788;
 
 attr_used attr_noreturn extern_C void kernel_main() {
   while (true) {
-    asm volatile("nop");
+    attr_maybe_unused u64 xx = xxx;
   }
 
   while (true) {}
