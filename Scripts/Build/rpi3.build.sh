b6592047b36ed576236e9c0c35ac853ec1c20006
shifu <shifu.wu@outlook.com>
2022-06-27 19:57:28 +0800

    [smoke-test] add smoke test

--- Scripts/Build/rpi3.build.sh
+++ Scripts/Build/rpi3.build.sh
@@ -16,21 +16,24 @@ function print_help() {
   echo "-h  show this help"
   echo "-d  build debug version"
   echo "-r  run qemu after build"
+  echo "-t  run smoke test after build"
   echo "-c  delete all cmake configuration caches"
   echo "-q  append qemu command args"
 
   exit 0
 }
 
+opt_smoke_test=0
 opt_run_qemu=0
 opt_reset_cmake=0
 opt_qemu_args=
 opt_build_type="Release"
 
-while getopts 'hdrcq:' OPT; do
+while getopts 'hdrtcq:' OPT; do
   case $OPT in
   d) opt_build_type="Debug" ;;
   r) opt_run_qemu=1 ;;
+  t) opt_smoke_test=1 ;;
   c) opt_reset_cmake=1 ;;
   q) opt_qemu_args=$OPTARG ;;
   h)
@@ -51,18 +54,28 @@ if [ ${opt_reset_cmake} == "1" ]; then
   rm -rf "${BUILD_DIR:?}"/*
 fi
 
-#todo: enable iwyu after the iwyu issue "Assertion `Loc.isValid()' failed" fixed
-cmake_config_command="cmake -DOPTION_DISABLE_IWYU=ON -DCMAKE_BUILD_TYPE=${opt_build_type} -DCMAKE_TOOLCHAIN_FILE=${THIS_DIR}/../../CMake/Toolchain/ToolchainClang.cmake -DCONFIG_BOARD=rpi3 -GNinja ${THIS_DIR}/../.."
+cmake_config_command="cmake -DCMAKE_BUILD_TYPE=${opt_build_type} -DCMAKE_TOOLCHAIN_FILE=${THIS_DIR}/../../CMake/Toolchain/ToolchainClang.cmake -DCONFIG_BOARD=rpi3 -GNinja ${THIS_DIR}/../.."
 echo "${cmake_config_command}"
 ${cmake_config_command}
 cmake_build_command="cmake --build . -j$(nproc)"
 echo "${cmake_build_command}"
-${cmake_build_command}
+eval "${cmake_build_command}"
+
+rpi3_qemu_command="qemu-system-aarch64 -M raspi3b -kernel bin/kernel.elf -semihosting -serial null -serial mon:stdio -nographic $opt_qemu_args"
+
+if [ ${opt_smoke_test} == "1" ]; then
+  msg="moss operating system"
+  smoke_test_command="timeout -s KILL 2 ${rpi3_qemu_command} | tee output.log || true"
+  echo 'running smote test'
+  echo "${smoke_test_command}"
+  echo "${smoke_test_command}" | eval "$(cat -)" 2>/dev/null
+  grep "${msg}" output.log > /dev/null
+  rm -f output.log
+fi
 
 if [ ${opt_run_qemu} == "1" ]; then
-  rpi3_qemu_command="qemu-system-aarch64 -M raspi3b -kernel bin/kernel.elf -semihosting -serial mon:null -serial mon:stdio -nographic $opt_qemu_args"
   echo "${rpi3_qemu_command}"
-  ${rpi3_qemu_command}
+  eval "${rpi3_qemu_command}"
 fi
 
 popd >/dev/null
--- SourceCode/Kernel/src/main.cc
+++ SourceCode/Kernel/src/main.cc
@@ -3,15 +3,10 @@
 #include "libcxx/types.hh"  // for paddr_t
 #include "libstd/stdio.hh"  // for printf
 
-static void print_test() {
-  int xx = 0;
-  // todo: %.2f not working
-  printf("welcome to moss, %s. %d, %08d, 0x%X, %f, %.2f, %c, %p\n", "hello world!", 123, 456, 0x123ab456, 0.123, 13.24,
-         'A', (void*)(&xx));
-}
+static void print_moss() { printf("moss operating system\n"); }
 
 extern_C attr_noreturn attr_used void kernel_main(attr_maybe_unused paddr_t handoff_paddr) {
-  print_test();
+  print_moss();
 
   while (true) {
     asm volatile("wfe");
