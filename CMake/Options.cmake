85573d9b60ebe11593258f73f94b41d1aafac4f3
shifu <shifu.wu@outlook.com>
2022-06-27 13:54:54 +0800

    [iwyu] add option `OPTION_IWYU_UPDATE_COMMENTS`

--- CMake/Options.cmake
+++ CMake/Options.cmake
@@ -1,2 +1,3 @@
 option(OPTION_DISABLE_CLANG_TIDY "disable clang-tidy" OFF)
 option(OPTION_DISABLE_IWYU "disable iwyu" OFF)
+option(OPTION_IWYU_UPDATE_COMMENTS "iwyu update comments" OFF)
--- CMake/Setup/IWYU.cmake
+++ CMake/Setup/IWYU.cmake
@@ -9,7 +9,10 @@ else ()
         message(FATAL_ERROR "could not find the program include-what-you-use")
     endif ()
 
-    set(__iwyu_options -Xiwyu;--cxx17ns;-Xiwyu;--no_default_mappings;-Xiwyu;--no_fwd_decls;)
+    if (OPTION_IWYU_UPDATE_COMMENTS)
+        set(iwyu_update_comments "-Xiwyu;--update_comments;")
+    endif ()
+    set(__iwyu_options -Xiwyu;--cxx17ns;-Xiwyu;--no_default_mappings;-Xiwyu;--no_fwd_decls;${iwyu_update_comments})
 
     function(target_enable_iwyu IN_target)
         set_target_properties(${IN_target} PROPERTIES CXX_INCLUDE_WHAT_YOU_USE "${OUT_path_to_iwyu};${__iwyu_options}")
@@ -26,11 +29,14 @@ else ()
         message(FATAL_ERROR "include-what-you-use dir could not be found, use `git pull --recurse-submodules` to pull submodules")
     endif ()
 
+    if (OPTION_IWYU_UPDATE_COMMENTS)
+        set(iwyu_update_comments_fix "--update_comments")
+    endif ()
     add_custom_target(iwyu_fix_includes
             COMMENT "iwyu fixing includes, wait a moment"
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
             DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
             COMMAND ${Python3_EXECUTABLE} ${MOSS_SCRIPTS_DIR}/IWYU/include-what-you-use/iwyu_tool.py -p ${CMAKE_BINARY_DIR} -- ${__iwyu_options} > ${CMAKE_BINARY_DIR}/.iwyu_fix_includes || (exit 0)
-            COMMAND ${Python3_EXECUTABLE} ${MOSS_SCRIPTS_DIR}/IWYU/include-what-you-use/fix_includes.py --reorder --comments < ${CMAKE_BINARY_DIR}/.iwyu_fix_includes || (exit 0)
+            COMMAND ${Python3_EXECUTABLE} ${MOSS_SCRIPTS_DIR}/IWYU/include-what-you-use/fix_includes.py --comments ${iwyu_update_comments_fix} < ${CMAKE_BINARY_DIR}/.iwyu_fix_includes || (exit 0)
             )
 endif ()
