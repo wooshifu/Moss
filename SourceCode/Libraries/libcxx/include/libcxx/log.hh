ad7f4052967d74fe0573f4776cb4aaee00840b9a
shifu <shifu.wu@outlook.com>
2022-07-05 20:20:59 +0800

    [log] add `__LOG_ARGS`

--- SourceCode/Libraries/libcxx/include/libcxx/log.hh
+++ SourceCode/Libraries/libcxx/include/libcxx/log.hh
@@ -39,7 +39,6 @@
 #define LOG_LEVEL_PANIC 8
 #endif
 
-#ifndef __LOG_FORMAT
 #if CONFIG_LOG_COLORFUL
 #define __LOG_COLORFUL_NOCOLOR
 #define __LOG_COLORFUL_RED               "\033[0;31m"
@@ -51,6 +50,7 @@
 #define __LOG_COLORFUL_CYAN              "\033[0;36m"
 #define __LOG_COLORFUL_RESET             "\033[0m"
 #define __LOG_FORMAT(prefix, fmt, color) color "[" #prefix " %s:%d] " fmt __LOG_COLORFUL_RESET "\n"
+#define __LOG_ARGS                       __FILE_NAME__, __LINE__
 #else
 #define __LOG_COLORFUL_NOCOLOR
 #define __LOG_COLORFUL_RED
@@ -62,12 +62,12 @@
 #define __LOG_COLORFUL_CYAN
 #define __LOG_COLORFUL_RESET
 #define __LOG_FORMAT(prefix, fmt, color) "[" #prefix " %s:%d] " fmt "\n"
-#endif
+#define __LOG_ARGS                       __FILE_NAME__, __LINE__
 #endif
 
 #ifndef log_t
 #if CONFIG_LOG_LEVEL <= LOG_LEVEL_TRACE
-#define log_t(fmt, ...) printf(__LOG_FORMAT(T, fmt, __LOG_COLORFUL_LIGHT_GRAY), __FILE_NAME__, __LINE__, ##__VA_ARGS__)
+#define log_t(fmt, ...) printf(__LOG_FORMAT(T, fmt, __LOG_COLORFUL_LIGHT_GRAY), __LOG_ARGS, ##__VA_ARGS__)
 #else
 #define log_t(fmt, ...)
 #endif
@@ -75,7 +75,7 @@
 
 #ifndef log_v
 #if CONFIG_LOG_LEVEL <= LOG_LEVEL_VERBOSE
-#define log_v(fmt, ...) printf(__LOG_FORMAT(V, fmt, __LOG_COLORFUL_LIGHT_GRAY), __FILE_NAME__, __LINE__, ##__VA_ARGS__)
+#define log_v(fmt, ...) printf(__LOG_FORMAT(V, fmt, __LOG_COLORFUL_LIGHT_GRAY), __LOG_ARGS, ##__VA_ARGS__)
 #else
 #define log_v(fmt, ...)
 #endif
@@ -83,7 +83,7 @@
 
 #ifndef log_d
 #if CONFIG_LOG_LEVEL <= LOG_LEVEL_DEBUG
-#define log_d(fmt, ...) printf(__LOG_FORMAT(D, fmt, __LOG_COLORFUL_LIGHT_GRAY), __FILE_NAME__, __LINE__, ##__VA_ARGS__)
+#define log_d(fmt, ...) printf(__LOG_FORMAT(D, fmt, __LOG_COLORFUL_LIGHT_GRAY), __LOG_ARGS, ##__VA_ARGS__)
 #else
 #define log_d(fmt, ...)
 #endif
@@ -91,7 +91,7 @@
 
 #ifndef log_i
 #if CONFIG_LOG_LEVEL <= LOG_LEVEL_INFO
-#define log_i(fmt, ...) printf(__LOG_FORMAT(I, fmt, __LOG_COLORFUL_GREEN), __FILE_NAME__, __LINE__, ##__VA_ARGS__)
+#define log_i(fmt, ...) printf(__LOG_FORMAT(I, fmt, __LOG_COLORFUL_GREEN), __LOG_ARGS, ##__VA_ARGS__)
 #else
 #define log_i(fmt, ...)
 #endif
@@ -99,7 +99,7 @@
 
 #ifndef log_w
 #if CONFIG_LOG_LEVEL <= LOG_LEVEL_WARNING
-#define log_w(fmt, ...) printf(__LOG_FORMAT(W, fmt, __LOG_COLORFUL_YELLOW), __FILE_NAME__, __LINE__, ##__VA_ARGS__)
+#define log_w(fmt, ...) printf(__LOG_FORMAT(W, fmt, __LOG_COLORFUL_YELLOW), __LOG_ARGS, ##__VA_ARGS__)
 #else
 #define log_w(fmt, ...)
 #endif
@@ -107,7 +107,7 @@
 
 #ifndef log_e
 #if CONFIG_LOG_LEVEL <= LOG_LEVEL_ERROR
-#define log_e(fmt, ...) printf(__LOG_FORMAT(E, fmt, __LOG_COLORFUL_RED), __FILE_NAME__, __LINE__, ##__VA_ARGS__)
+#define log_e(fmt, ...) printf(__LOG_FORMAT(E, fmt, __LOG_COLORFUL_RED), __LOG_ARGS, ##__VA_ARGS__)
 #else
 #define log_e(fmt, ...)
 #endif
@@ -115,7 +115,7 @@
 
 #ifndef log_f
 #if CONFIG_LOG_LEVEL <= LOG_LEVEL_FATAL_ERROR
-#define log_f(fmt, ...) printf(__LOG_FORMAT(F, fmt, __LOG_COLORFUL_PURPLE), __FILE_NAME__, __LINE__, ##__VA_ARGS__)
+#define log_f(fmt, ...) printf(__LOG_FORMAT(F, fmt, __LOG_COLORFUL_PURPLE), __LOG_ARGS, ##__VA_ARGS__)
 #else
 #define log_f(fmt, ...)
 #endif
@@ -123,7 +123,7 @@
 
 #ifndef log_p
 #if CONFIG_LOG_LEVEL <= LOG_LEVEL_PANIC
-#define log_p(fmt, ...) printf(__LOG_FORMAT(P, fmt, __LOG_COLORFUL_PURPLE), __FILE_NAME__, __LINE__, ##__VA_ARGS__)
+#define log_p(fmt, ...) printf(__LOG_FORMAT(P, fmt, __LOG_COLORFUL_PURPLE), __LOG_ARGS, ##__VA_ARGS__)
 #else
 #define log_p(fmt, ...)
 #endif
